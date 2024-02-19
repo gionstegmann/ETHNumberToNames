43288e81ae731c1438e1c533bbbf16746c3a04ee
Gion Stegmann <gion.stegmann@gmail.com>
2024-02-19 19:18:41 +0100

    added some courses

--- content.js
+++ content.js
@@ -6,9 +6,24 @@ const courseMappings = {
     "252-0210-00L": { "full": "Compiler Design", "short": "CompDes" },
     "252-0206-00L": { "full": "Visual Computing", "short": "VisComp" },
     "401-0663-00L": { "full": "Numerical Methods for Computer Science", "short": "NumCS" },
-    "252-0028-00L": { "full": "Design of Digital Circuits", "short": "Digitech" }
-     
+    "252-0028-00L": { "full": "Design of Digital Circuits", "short": "Digitech" },
+    "252-0058-00L": { "full": "Formal Methods and Functional Programming", "short": "FMFP" },
+    "401-0212-16L": { "full": "Analysis I" , "short": "Ana1" },
+    "227-0003-10L": { "full": "Digital Design and Computer Architecture" , "short": "Digitech" },
+    "252-0029-00L": { "full": "Parallele Programmierung", "short": "PProg" },
+    "252-0030-00L": { "full": "Algorithmen und Wahrscheinlichkeit ", "short": "A&W" },
+    "252-0063-00L": { "full": "Data Modelling and Databases", "short": "DMDB" },
+    "252-0064-00L": { "full": "Computer Networks", "short": "CompNet" },
+    "252-0220-00L": { "full": "Introduction to Machine Learning", "short": "IML" },
+    "401-0614-00L": { "full": "Wahrscheinlichkeit und Statistik", "short": "W&S" },
+    "261-5110-00L": { "full": "Optimization for Data Science", "short": "ODS" }, //dunno if the short is correct
+    "263-3710-00L": { "full": "Machine Perception", "short": "MP" }, //dunno if the short is correct
+    "263-4660-00L": { "full": "Applied Cryptography", "short": "AppCrypt" }, //dunno if the short is correct
+    "252-0535-00L": { "full": "Advanced Machine Learning", "short": "AML" },
+
     // Add more mappings here
+    // TODO: find a way to scrape this data from the course catalogue
+    // TODO: put this db in different file
 };
 
 function replaceCourseCodes(textNode, nameStyle, addBrackets) {
